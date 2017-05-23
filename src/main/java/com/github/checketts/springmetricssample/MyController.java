8ead9b9d68b19819f310337a53809d4cb36e28e6
Clint Checketts <clint.checketts@domo.com>
2017-05-23 02:10:49 -0600

    Add executor examples

---
+++ src/main/java/com/github/checketts/springmetricssample/MyController.java
@@ -0,0 +1,82 @@
+package com.github.checketts.springmetricssample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.metrics.annotation.Timed;
+import org.springframework.metrics.instrument.Counter;
+import org.springframework.metrics.instrument.MeterRegistry;
+import org.springframework.metrics.instrument.Timer;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by clintchecketts on 5/23/17.
+ */
+@RestController
+public class MyController {
+  private static final Logger LOG = LoggerFactory.getLogger(MyController.class);
+  private final TaskExecutor executor;
+  private final TaskScheduler sched;
+  List<String> people = new ArrayList<>();
+  Counter steveCounter;
+  Timer findPersonTimer;
+  Timer schedTimer;
+
+  @Scheduled(fixedRate = 5000)
+  @Timed(extraTags = {"name", "bob"})
+  public void runMe() {
+    schedTimer.record(() -> LOG.info("Running the runMe"));
+  }
+
+  public MyController(MeterRegistry registry,
+                      @Qualifier("tpExec") TaskExecutor executor,
+                      @Qualifier("tpSched") TaskScheduler sched) {
+    this.executor = executor;
+    this.sched = sched;
+
+    // registers a gauge to observe the size of the population
+    registry.collectionSize("population", people);
+
+    // register a counter of questionable usefulness
+    steveCounter = registry.counter("find_steve");
+
+    // register a timer -- though for request timing it is easier to use @Timed
+    findPersonTimer = registry.timer("http_requests", "method", "GET");
+
+    schedTimer = registry.timer("sample_bob_scheduled");
+  }
+
+  @GetMapping("/api/person")
+  public String findPerson(@RequestParam String q) throws Throwable {
+    for (int i = 0; i < 30; i++) {
+      final int j = i;
+      sched.scheduleAtFixedRate(() -> {
+        LOG.info("Fixed rate: " + j);
+      }, 1000);
+    }
+
+    for (int i = 0; i < 30; i++) {
+      final int j = i;
+      executor.execute(() -> {
+        LOG.info("Execute: " + j);
+      });
+    }
+
+
+    return findPersonTimer.recordThrowable(() -> { // use the timer!
+      if (q.toLowerCase().contains("steve")) {
+        steveCounter.increment(); // use the counter
+      }
+
+      return people.stream().filter(p -> q.equals(p)).findAny().orElse(null);
+    });
+  }
+}
--- src/main/java/com/github/checketts/springmetricssample/SpringMetricsSampleApplication.java
+++ src/main/java/com/github/checketts/springmetricssample/SpringMetricsSampleApplication.java
@@ -1,16 +1,26 @@
 package com.github.checketts.springmetricssample;
 
-import io.prometheus.client.CollectorRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.metrics.annotation.Timed;
 import org.springframework.metrics.instrument.Counter;
 import org.springframework.metrics.instrument.MeterRegistry;
 import org.springframework.metrics.instrument.Timer;
 import org.springframework.metrics.instrument.binder.LogbackMetrics;
-import org.springframework.metrics.instrument.binder.MetricsTurboFilter;
 import org.springframework.metrics.instrument.prometheus.EnablePrometheusScraping;
 import org.springframework.metrics.instrument.prometheus.PrometheusMeterRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,44 +30,53 @@ import java.util.List;
 
 @SpringBootApplication
 @EnablePrometheusScraping
+@EnableScheduling
+@EnableAspectJAutoProxy
 public class SpringMetricsSampleApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(SpringMetricsSampleApplication.class);
 
     @Bean
-    public MeterRegistry meterRegistry() { return new PrometheusMeterRegistry(); }
+    public MeterRegistry meterRegistry() {
+        return new PrometheusMeterRegistry();
+    }
 
     @Bean
     public LogbackMetrics logbackMetrics() {
         return new LogbackMetrics();
     }
 
-    @RestController
-    public class MyController {
-        List<String> people = new ArrayList<String>();
-        Counter steveCounter;
-        Timer findPersonTimer;
+    @Bean
+    public ThreadPoolTaskExecutor tpExec(MeterRegistry registry){
+        ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
+        exec.setCorePoolSize(10);
+        exec.setMaxPoolSize(10);
+        exec.initialize();
 
-        public MyController(MeterRegistry registry) {
-            // registers a gauge to observe the size of the population
-            registry.collectionSize("population", people);
-
-            // register a counter of questionable usefulness
-            steveCounter = registry.counter("find_steve");
-
-            // register a timer -- though for request timing it is easier to use @Timed
-            findPersonTimer = registry.timer("http_requests", "method", "GET");
-        }
-
-        @GetMapping("/api/person")
-        public String findPerson(@RequestParam String q) throws Exception {
-            return findPersonTimer.record(() -> { // use the timer!
-                if(q.toLowerCase().contains("steve")) {
-                    steveCounter.increment(); // use the counter
-                }
-
-                return people.stream().filter(p -> q.equals(p)).findAny().orElse(null);
-            });
-        }
+        Timer t = registry.timer("tp_exec");
+        registry.monitor("tp_exec_exec", exec.getThreadPoolExecutor());
+        exec.setTaskDecorator(new TaskDecorator() {
+            @Override
+            public Runnable decorate(Runnable runnable) {
+                return t.wrap(runnable);
+            }
+        });
+        return exec;
     }
+
+
+    @Bean
+    public ThreadPoolTaskScheduler tpSched(MeterRegistry registry){
+        ThreadPoolTaskScheduler exec = new ThreadPoolTaskScheduler();
+        exec.initialize();
+
+        registry.monitor("tp_wsched", exec.getScheduledThreadPoolExecutor());
+        //Give to metric registry to measure? exec.getScheduledThreadPoolExecutor();
+        exec.setPoolSize(10);
+        exec.initialize();
+        return exec;
+    }
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMetricsSampleApplication.class, args);
